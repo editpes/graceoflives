bd9d0ec44f72f70eee59a6d5bbc121c084290979
editpes <editpes@yahoo.co.id>
2016-11-19 04:01:26 +0700

    change rawgit

--- scripts/graceoflives.js
+++ scripts/graceoflives.js
@@ -654,7 +654,7 @@ function showBossRaidData() {
 
 //ELEMENTS' BEHAVIORS
 $(document).ready(function() {
-  $("head").append("<link id=\"themeLink\" rel=\"stylesheet\" href=\"https://cdn.rawgit.com/graceoflives/fluffy-garbanzo/master/theme_dark.css\" type=\"text/css\" />");
+  $("head").append("<link id=\"themeLink\" rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/graceoflives/fluffy-garbanzo/master/theme_dark.css" type=\"text/css\" />");
   $("#themeChoice input").on("change", function() {
     $("head #themeLink").prop("href", "https://cdn.rawgit.com/graceoflives/fluffy-garbanzo/master/theme_" + $("#themeChoice input[name=\"theme\"]:checked").val() + ".css");
   });
